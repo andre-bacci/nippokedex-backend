f34192e17199946a97ca8ae27b9e1448d06667cc
andre-bacci <avfbacci@gmail.com>
2025-04-24 19:26:29 -0300

    minor

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,18 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts}"],
+    files: ['**/*.{js,mjs,cjs,ts}'],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
   },
   {
-    files: ["**/*.{js,mjs,cjs,ts}"],
+    files: ['**/*.{js,mjs,cjs,ts}'],
     languageOptions: { globals: globals.node },
   },
-  { rules: { "@typescript-eslint/no-require-imports": "off" } },
+  { rules: { '@typescript-eslint/no-require-imports': 'off' } },
   tseslint.configs.recommended,
 ]);
